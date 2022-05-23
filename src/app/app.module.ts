8fd93d45e92b728de740759baff744f0c6b6f0e2
andres <andres06energy@hotmail.com>
2022-05-23 16:51:01 -0300

    Updated Proyect

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarModule } from './components/navbar/navbar.module';
+import { FooterComponent } from './components/footer/footer.component';
+import { FooterModule } from './components/footer/footer.module';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { CarouselModule } from './components/carousel/carousel.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CarouselComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NavbarModule,
+    FooterModule,
+    CarouselModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
