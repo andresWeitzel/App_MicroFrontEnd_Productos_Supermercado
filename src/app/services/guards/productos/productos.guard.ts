e9c071291e7d1b9bd8eee6028572697f1379ed91
Andrés Weitzel <andres96energy@hotmail.com>
2022-08-01 16:55:31 -0300

    Updated enviroments

---
+++ src/app/services/guards/productos/productos.guard.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { TokenService } from '../../token/token.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosGuard implements CanActivate {
+
+
+  rol:string;
+
+  constructor(
+    private tokenService:TokenService,
+    private router:Router
+  ) { }
+
+
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean  {
+
+      const expectedRol = route.data['expectedRol'];
+
+
+      this.rol = this.tokenService.isAdmin() ? 'admin' : 'user';
+
+
+      if(!(this.tokenService.isLogged())
+      || (expectedRol.indexOf(this.rol) < 0)
+      ){
+        //this.router.navigate(['/login']);
+        this.router.navigate(['/']);
+        return false;
+      }
+      return true;
+
+
+  }
+
+}
--- src/app/services/producto/producto.service.ts
+++ src/app/services/producto/producto.service.ts
@@ -2,9 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ProductoDto } from 'src/app/models/ProductoDto';
+import { environment } from 'src/environments/environment';
 
-
-const URL_PRODUCTO_BASE = 'http://localhost:8098/api/v1/productos/';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +11,7 @@ const URL_PRODUCTO_BASE = 'http://localhost:8098/api/v1/productos/';
 export class ProductoService {
 
 
+  productoUrlApi = environment.URL_PRODUCTO_API;
 
   constructor(
       private httpClient:HttpClient
@@ -19,39 +19,39 @@ export class ProductoService {
   ) { }
 
 
+
   //================ METODOS CRUD ====================
 
 
   //--- GET ALL ---
   public listado(nroPagina:number , nroElementos:number , orderType:string, orderBy:string):Observable<ProductoDto[]>{
-    return this.httpClient.get<any>(`${URL_PRODUCTO_BASE}listado?page=${nroPagina}&size=${nroElementos}&sort=${orderType},${orderBy}`);
+    return this.httpClient.get<any>(`${this.productoUrlApi}listado?page=${nroPagina}&size=${nroElementos}&sort=${orderType},${orderBy}`);
   }
 
 //--- GET ALL FILTER---
   public listadoFilter(filtro:string, nroPagina:number , nroElementos:number , orderType:string, orderBy:string ):Observable<ProductoDto[]>{
-    return this.httpClient.get<any>(`${URL_PRODUCTO_BASE}listado-filter/${filtro}?page=${nroPagina}&size=${nroElementos}&sort=${orderType},${orderBy}`);
+    return this.httpClient.get<any>(`${this.productoUrlApi}listado-filter/${filtro}?page=${nroPagina}&size=${nroElementos}&sort=${orderType},${orderBy}`);
   }
 
 //--- ADD ---
 public add(producto:ProductoDto):Observable<ProductoDto>{
-  return this.httpClient.post<any>(`${URL_PRODUCTO_BASE}`,producto);
+  return this.httpClient.post<any>(`${this.productoUrlApi}`,producto);
 }
 
 //--- UPDATE ---
   public update(id:number, producto:ProductoDto):Observable<ProductoDto>{
-    return this.httpClient.put<any>(`${URL_PRODUCTO_BASE}${id}`,producto);
+    return this.httpClient.put<any>(`${this.productoUrlApi}${id}`,producto);
 
   }
 
   //--- DELETE ---
   public delete(id:number):Observable<ProductoDto>{
-    return this.httpClient.delete<any>(`${URL_PRODUCTO_BASE}${id}`);
+    return this.httpClient.delete<any>(`${this.productoUrlApi}${id}`);
 
   }
 
 
   //================ METODOS DE BUSQUEDA ====================
-
 
 
 }
